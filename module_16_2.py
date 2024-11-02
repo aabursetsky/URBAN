ee6aa7d14832810d2a309993a1b2305ff4bc1e79
aabursetsky <114884761+aabursetsky@users.noreply.github.com>
2024-11-02 14:22:17 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get_main_page() -> dict:
+    """
+    Главная страница
+    """
+    return {"message": "Главная страница"}
+
+
+@app.get("/user/{user_id}")
+async def get_user_number(user_id: int = Path(ge=1, le=100, description='Enter User ID', example=13)) -> dict:
+    """
+    Вы вошли как пользователь № <user_id>
+    :param user_id:
+    """
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user/{username}/{age}")
+async def get_user_info(username: Annotated[str, Path(
+    min_length=5,
+    max_length=20,
+    description='Enter username',
+    example='UrbanUser')],
+                            age: Annotated[int, Path(
+    ge=18,
+    le=120,
+    description='Enter age',
+    example='24')]) -> dict:
+    """
+    Информация о пользователе. Имя: <username>, Возраст: <age>
+    :param username:
+    :param age:
+    """
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}."}
+
+
+@app.get("/user/admin")
+async def get_admin_page() -> dict:
+    """
+    Вы вошли как администратор
+    """
+    return {"message": "Вы вошли как администратор"}
