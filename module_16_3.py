2dbaa49c76efcb9b619a4b250d5d0390b5cfe605
aabursetsky <114884761+aabursetsky@users.noreply.github.com>
2024-11-06 23:36:52 +0300

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+# Вывод данных о пользователях
+@app.get("/users")
+async def get_all_users() -> dict:
+    return users
+
+# Создание нового пользователя
+@app.post("/user/{username}/{age}")
+async def create_user(username: Annotated[str, Path(min_length=5,
+                                                      max_length=20,
+                                                      description='Enter username',
+                                                      example='UrbanUser')],
+                      age: int = Path(ge=18,
+                                        le=120,
+                                        description='Enter age',
+                                        example='24')) -> str:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
+
+# Обновление данных о пользователе
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(user_id: str, username: Annotated[str, Path(min_length=5,
+                                                      max_length=20,
+                                                      description='Enter username',
+                                                      example='UrbanUser')],
+                      age: int = Path(ge=18,
+                                        le=120,
+                                        description='Enter age',
+                                        example='24')) -> str:
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user {user_id} is updated"
+
+# Удаление пользователя
+@app.delete("/user/{user_id}")
+async def delete_user(user_id: str) -> str:
+    if user_id in users:
+        users.pop(user_id)
+        return f"User {user_id} has been deleted"
+    else:
+        return "Пользователь не найден."
+
